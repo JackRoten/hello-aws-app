c70e4fc8bc709f20b82f2b4e228e4031bb261a01
JackRoten <thejackroten@gmail.com>
2026-02-02 21:43:43 -0800

    Add var names to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,16 @@ variable "region" {
   default = "us-west-2"
 }
 
-variable "db_user" {}
-variable "db_pass" {}
+variable "db_user" {
+  default = var.db_user
+}
+variable "db_pass" {
+  default = var.db_pass
+}
 
-variable "backend_image" {}
-variable "frontend_image" {}
+variable "backend_image" {
+  default = var.backend_image
+}
+variable "frontend_image" {
+  default = var.frontend_image
+}
