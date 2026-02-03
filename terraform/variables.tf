58ba154e6f01ca430762534e6a069dfc74d8cfe3
JackRoten <thejackroten@gmail.com>
2026-02-02 21:52:20 -0800

    Add default front/backend image names to varaiables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,16 +2,12 @@ variable "region" {
   default = "us-west-2"
 }
 
-variable "db_user" {
-  default = var.db_user
-}
-variable "db_pass" {
-  default = var.db_pass
-}
+variable "db_user" {}
+variable "db_pass" {}
 
 variable "backend_image" {
-  default = var.backend_image
+  default = "backend:latest"
 }
 variable "frontend_image" {
-  default = var.frontend_image
+  default = "frontend:latest"
 }
